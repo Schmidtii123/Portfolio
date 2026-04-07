72b156828a507f305c55e3e2539d2595c1e3540b
Schmidtii123 <schmidtii2000@gmail.com>
2026-04-07 12:11:58 +0200

    Feature - Add new project from my last exam - and change the order of projects shown from first to last - add some comments to get a better sens of what project it is

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,33 +3,71 @@ export default function Projects() {
         <article id='project'>
             <h2 className="title">Projekter</h2>
             <div className="projects">
-                <div className="project1" data-aos="fade-left">
+
+                {/* ------------------------------------------ Crit Card Afslutende eksamen ------------------------------------*/}
+                <div className="project1" data-aos="fade-right">
                     <div>
                         <div className="img_container project_github">
-                            <a href="https://awu-exam-schmidtii123.onrender.com/" target="_blank">
-                                <img className="projects_img" src="./Bookify-3-semester.png" alt="Billede af forsiden til den gamle slagter keramik" />
+                            <a href="https://chipper-moonbeam-100da6.netlify.app/" target="_blank">
+                                <img className="projects_img" src="./crit_card.png" alt="Billede af forsiden til den gamle slagter keramik" />
                             </a>
 
                         </div>
                     </div>
                     <div className="title_text">
                         <h3>
-                            Bookify - Bog app/hjemmeside til telefon
+                            Crit Card - Kickstarter
                         </h3>
-                        <p>I 2 semester af webudvikling fik vi til opgave at lave en Bog side ved brug af typescript og React router 7, Der var nogle krav som skulle overholdes. Den er kun lavet til mobil, og er derfor ikke responsiv.
+                        <p>På mit afsluttende semester udviklede jeg en webapp som et koncept til en “Crit Cards” platform til DnD-spillere. Her kan brugere oprette og joine en lobby via ID eller link, hvorefter en DM styrer spillet og trækker kort i realtid. Kortene hentes fra databasen og vises for alle spillere samtidig ved brug af realtidskommunikation (pub/sub). <br /> <br />
+
+                        Applikationen er bygget med Vue/TypeScript i frontend og PHP Laravel i backend samt pusher til realtidskommunikation. Projektet er et udkast til, hvordan platformen kunne fungere, og er derfor ikke live. Jeg opnåede karakteren 10 for projektet.
 
                         </p>
                         <div className="github_code">
 
-                            <a className="github" href="https://github.com/eaaa-dob-wu-e24a/awu-exam-Schmidtii123" target="_blank">
+                            <a className="github" href="https://github.com/Schmidtii123/kortspil-backend" target="_blank">
                                 <img className="github_img" src="./github.png" alt="Billede af Gibhub logoet" />Kode</a>
-                            <a className="code" href="https://awu-exam-schmidtii123.onrender.com/" target="_blank" rel="noreferrer">
+                            <a className="code" href="https://chipper-moonbeam-100da6.netlify.app/" target="_blank" >
                                 <img className="github_img" src="./demo_img.png" alt="Billede af en computer med en demo" />
                                 Live Demo
                             </a>
                         </div>
                     </div>
+
                 </div>
+
+
+                {/* ------------------------------------------ BookBazr - Webapp ----------------------------------------*/}
+                <div className="project1" data-aos="fade-left">
+                    <div className="title_text">
+                        <h3>
+                            BookBazr - Webapp
+                        </h3>
+                        <p>I 3 semester fik vi til opgave at lave en webapp, her er en webapp, der kan hjælpe studerende med at sælge og købe brugte studiebøger, så de kan spare penge. Der er mulighed for at chatte, og med brug af ISBN nummer henter vi bogens information fra en google API, Den er kun lavet til mobil, og er derfor ikke responsiv.
+
+                        </p>
+                        <div className="github_code">
+
+                            <a className="github" href="https://github.com/Schmidtii123/webapp" target="_blank">
+                                <img className="github_img" src="./github.png" alt="Billede af Gibhub logoet" />Kode</a>
+                            <a className="code" href="https://bookbazr.vercel.app/" target="_blank" >
+                                <img className="github_img" src="./demo_img.png" alt="Billede af en computer med en demo" />
+                                Live Demo
+                            </a>
+                        </div>
+                    </div>
+
+                    <div>
+                        <div className="img_container project_github">
+                            <a href="https://bookbazr.vercel.app/" target="_blank">
+                                <img className="projects_img" src="./BookBazr.png" alt="Billede af forsiden til den gamle slagter keramik" />
+                            </a>
+
+                        </div>
+                    </div>
+
+                </div>
+                {/* ------------------------------------------ BookByNight - Webapp ----------------------------------------*/}
 
                 <div className="project1" data-aos="fade-right">
                     <div>
@@ -67,38 +105,38 @@ export default function Projects() {
 
 
                 </div>
-
+                {/* ------------------------------------------ Bookify - Telefonapp ----------------------------------------*/}
                 <div className="project1" data-aos="fade-left">
+                    <div>
+                        <div className="img_container project_github">
+                            <a href="https://awu-exam-schmidtii123.onrender.com/" target="_blank">
+                                <img className="projects_img" src="./Bookify-3-semester.png" alt="Billede af forsiden til den gamle slagter keramik" />
+                            </a>
+
+                        </div>
+                    </div>
                     <div className="title_text">
                         <h3>
-                            BookBazr - Webapp
+                            Bookify - Bog app/hjemmeside til telefon
                         </h3>
-                        <p>I 3 semester fik vi til opgave at lave en webapp, her er en webapp, der kan hjælpe studerende med at sælge og købe brugte studiebøger, så de kan spare penge. Der er mulighed for at chatte, og med brug af ISBN nummer henter vi bogens information fra en google API, Den er kun lavet til mobil, og er derfor ikke responsiv.
+                        <p>I 2 semester af webudvikling fik vi til opgave at lave en Bog side ved brug af typescript og React router 7, Der var nogle krav som skulle overholdes. Den er kun lavet til mobil, og er derfor ikke responsiv.
 
                         </p>
                         <div className="github_code">
 
-                            <a className="github" href="https://github.com/Schmidtii123/webapp" target="_blank">
+                            <a className="github" href="https://github.com/eaaa-dob-wu-e24a/awu-exam-Schmidtii123" target="_blank">
                                 <img className="github_img" src="./github.png" alt="Billede af Gibhub logoet" />Kode</a>
-                            <a className="code" href="https://bookbazr.vercel.app/" target="_blank" >
+                            <a className="code" href="https://awu-exam-schmidtii123.onrender.com/" target="_blank" rel="noreferrer">
                                 <img className="github_img" src="./demo_img.png" alt="Billede af en computer med en demo" />
                                 Live Demo
                             </a>
                         </div>
                     </div>
-
-                    <div>
-                        <div className="img_container project_github">
-                            <a href="https://bookbazr.vercel.app/" target="_blank">
-                                <img className="projects_img" src="./BookBazr.png" alt="Billede af forsiden til den gamle slagter keramik" />
-                            </a>
-
-                        </div>
-                    </div>
-
                 </div>
 
-                <div className="project2" data-aos="fade-right">
+                {/* ------------------------------------------ Keramik - Eksamensprojekt ---------------------------------------*/}
+
+                <div className="project1" data-aos="fade-right">
 
                     <div className="img_container project_github">
                         <a href="https://keramik-2-semester-eksamen.vercel.app/" target="_blank">
@@ -127,7 +165,8 @@ export default function Projects() {
 
                     </div>
                 </div>
-                <div className="project1" data-aos="fade-left">
+                {/* ------------------------------------------ Mark Ebert Webdoc ----------------------------------------*/}
+                <div className="project2" data-aos="fade-left">
                     <div className="title_text">
                         <h3>
                             Mark Ebert Webdoc
